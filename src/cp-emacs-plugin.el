754c36383617ee4931681fa7fef66fb061bd59a3
SecondCosmicV <secondcosmicv@gmail.com>
2026-03-04 22:20:37 +0100

    make sure test buff wd is where main is

--- src/cp-emacs-plugin.el
+++ src/cp-emacs-plugin.el
@@ -10,24 +10,26 @@
   (compile "g++ -g -Wall -o main main.cpp"))
 (defun cp/test ()
   (interactive)
-  (let ((buff (get-buffer-create "cp-test-results")))
+  (let ((buff (get-buffer-create "*cp-test-results*")))
     (display-buffer buff)
-    (with-current-buffer buff
-      (erase-buffer)
-      (let ((test-case 1))
-        (while (file-exists-p (format "in%d.txt" test-case))
-          (insert (format "%d. " test-case))
-          (let ((actual (shell-command-to-string (format "./main < in%d.txt" test-case)))
-                (correct (with-temp-buffer
-                           (insert-file-contents (format "out%d.txt" test-case))
-                           (buffer-string))))
-            (if (string-equal
-                  (string-trim actual)
-                  (string-trim correct))
-              (insert "AC")
-              (insert "WA")))
-            (insert "\n")
-          (setq test-case (+ test-case 1)))))))
+    (let ((wd default-directory))
+      (with-current-buffer buff
+        (setq-local default-directory wd)
+        (erase-buffer)
+        (let ((test-case 1))
+          (while (file-exists-p (format "in%d.txt" test-case))
+            (insert (format "%d. " test-case))
+            (let ((actual (shell-command-to-string (format "./main < in%d.txt" test-case)))
+                  (correct (with-temp-buffer
+                             (insert-file-contents (format "out%d.txt" test-case))
+                             (buffer-string))))
+              (if (string-equal
+                    (string-trim actual)
+                    (string-trim correct))
+                (insert "AC")
+                (insert "WA")))
+              (insert "\n")
+            (setq test-case (+ test-case 1))))))))
 (defvar cp/cp-mode-map
   (let ((map (make-sparse-keymap)))
     (define-key map (kbd "<f5>") 'cp/test)
